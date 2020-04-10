5e93e4e81bcc541284ab0c9ddddc1e0a2ca6fb37
Hellowalls1 <thesingingbutcher@gmail.com>
2020-04-10 15:47:28 -0500

    got the nurserys to populate

--- scripts/retailers/Retailer.js
+++ scripts/retailers/Retailer.js
@@ -1,5 +1,5 @@
 //HTML representation of flower data
-export const retailer = (retailerObject, distributorObject) => {
+export const retailer = (retailerObject, distributorObject, nurseriesArray) => {
     return `
         <section class="retailer">
             <section>
@@ -8,6 +8,14 @@ export const retailer = (retailerObject, distributorObject) => {
             <section class="retailer_location">${retailerObject.city}, ${retailerObject.state}</section>
             <section class="retailer_address">${retailerObject.address}</section>
             <section class="retailer_distributor">${distributorObject.name}</section>
+            <h3>We Proudly Support:</h3>
+            <section class="distributor_nurseries">
+            <ul>
+            ${
+                nurseriesArray.map(nursery => `<li>${nursery.name}</li>`).join("") //have to use .map on customerArray because the array is full of objects and must convert to display
+            }
+            </ul>
+            </section>
         </section>
     `
 }
